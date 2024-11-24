d0460046bb768662a8f34032997f9144772c4db3
Tyler Liu <tyler4long@gmail.com>
2024-11-23 20:55:06 -0800

    Updates

--- architecture-I3QFYML2-BYLGXPH6.js
+++ architecture-I3QFYML2-BYLGXPH6.js
@@ -1,2 +1,2 @@
-var e=globalThis.parcelRequirebb86;(0,e.register)("934lS",function(r,t){Object.defineProperty(r.exports,"createArchitectureServices",{get:()=>e("58fUp").createArchitectureServices,set:void 0,enumerable:!0,configurable:!0}),e("58fUp"),e("bYG2S"),e("76RTQ"),e("f34bP"),e("2266r"),e("7xxrv")});
+var e=globalThis.parcelRequire94c2;(0,e.register)("934lS",function(r,c){Object.defineProperty(r.exports,"createArchitectureServices",{get:()=>e("58fUp").createArchitectureServices,set:void 0,enumerable:!0,configurable:!0}),e("58fUp"),e("bYG2S"),e("76RTQ"),e("f34bP"),e("2266r"),e("7xxrv")});
 //# sourceMappingURL=architecture-I3QFYML2-BYLGXPH6.js.map
