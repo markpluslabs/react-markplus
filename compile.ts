7e5c6ce4e49a4cb2e2d51858340d5f304827b210
Tyler Liu <tyler4long@gmail.com>
2024-12-21 19:00:41 -0800

    Update compile script

--- compile.ts
+++ compile.ts
@@ -3,9 +3,15 @@ import { run } from "shell-commands";
 // deno requires local import paths to have the .ts extension
 // tsc requires local import paths to have the .js extension
 const main = async () => {
+  const changes = await run("git status --porcelain");
+  if (changes.trim().length > 0) {
+    console.log("Please commit your changes before running this script");
+    return;
+  }
   await run(
     `find src -type f \\( -name "*.ts" -o -name "*.tsx" \\) -exec sed -E -i '' 's/from "(\\.\\.?\\/.*)\\.ts(x)?";/from "\\1.js";/g' {} +`,
   );
   await run("yarn tsc");
+  await run("git stash && git stash clear");
 };
 main();
