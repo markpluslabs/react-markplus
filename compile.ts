164aecb98722c56655bc119c04e98df3653b72cf
Tyler Liu <tyler4long@gmail.com>
2024-12-21 18:45:56 -0800

    Add a compile script

---
+++ compile.ts
@@ -0,0 +1,13 @@
+import { run } from "shell-commands";
+
+// deno requires local import paths to have the .ts extension
+// tsc requires local import paths to have the .js extension
+const main = async () => {
+  await run(
+    `
+    find src -type f \\( -name "*.ts" -o -name "*.tsx" \\) -exec sed -i '' 's/from "\\(\\.\\/.*\\)\\.ts\\(x\\)?";/from "\\1.js";/g' {} +
+    `,
+  );
+  await run("yarn tsc");
+};
+main();
