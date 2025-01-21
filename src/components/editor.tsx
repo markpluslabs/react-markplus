a6e5e494a53d69b1d3ab7866cf30595e0eafde41
Tyler Liu <tyler4long@gmail.com>
2025-01-21 11:29:12 -0800

    scrollPastEnd

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -14,6 +14,7 @@ import {
   highlightActiveLine,
   KeyBinding,
   keymap,
+  scrollPastEnd,
   ViewUpdate,
 } from "@codemirror/view";
 import debounce from "debounce";
@@ -66,6 +67,7 @@ const Editor = (props: { store: Store }) => {
       extensions: [
         EditorView.lineWrapping,
         highlightActiveLine(),
+        scrollPastEnd(),
         history(),
         keymap.of([
           ...defaultKeymap,
